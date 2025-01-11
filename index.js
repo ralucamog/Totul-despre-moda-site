a545d86f26f23f4dca1a1cd742f3b293f3eb6df1
Raluca Mogîldea <162919070+ralucamog@users.noreply.github.com>
2025-01-11 17:01:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const port = 8080;
+
+app = express();
+console.log("Folder index.js", __dirname);
+console.log("Cale fisier", __filename);
+console.log("Folder curent (de lucru)", process.cwd());
