247a65a3c100037dbdb96e1a2d364bb922d9fd81
Raluca Mogîldea <162919070+ralucamog@users.noreply.github.com>
2025-01-11 17:02:46 +0200

    Add files via upload

---
+++ dist/index.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var express = require("express");
+
+var port = 8080;
+app = express();
+console.log("Folder index.js", __dirname);
+console.log("Cale fisier", __filename);
+console.log("Folder curent (de lucru)", process.cwd());
+//# sourceMappingURL=index.dev.js.map
